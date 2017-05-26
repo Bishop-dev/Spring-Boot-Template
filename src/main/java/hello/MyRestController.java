6b9f44718e4546981248d6fe83b7dfcdd6381c44
Bishop-dev <alex.bishop@yandex.ru>
2017-05-26 16:42:23 +0300

    Development mode
    works with IDEA in browser

---
+++ src/main/java/hello/MyRestController.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class MyRestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public Message testJson() {
+        return new Message("test message!");
+    }
+
+    static class Message {
+        private String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
+}
